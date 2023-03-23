30e01adad00c1e6557313e71e6a91cafb718b63b
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2023-03-23 17:11:57 +1000

    disabled imds for aws cli

--- homes/shared/default.nix
+++ homes/shared/default.nix
@@ -9,6 +9,9 @@
     username = "nixos";
     homeDirectory = "/home/nixos";
 
+    sessionVariables = {
+      AWS_EC2_METADATA_DISABLED = "true";
+    };
     # This value determines the Home Manager release that your
     # configuration is compatible with. This helps avoid breakage
     # when a new Home Manager release introduces backwards
