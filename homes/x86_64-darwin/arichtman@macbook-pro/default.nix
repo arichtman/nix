9550c6b86d3e8d34f3e38920f3a477d9fbb86eac
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-06-20 23:46:17 +1000

    who knows

--- homes/x86_64-darwin/arichtman@macbook-pro/default.nix
+++ homes/x86_64-darwin/arichtman@macbook-pro/default.nix
@@ -1,20 +1,19 @@
 { lib, pkgs, config, ... }:
 let
   myShellAliases = {
-    ll = "ls -hAlLrt";
     "brute-force-darwin-rebuild-switch" =  "until darwin-rebuild switch --flake . ; do : ; done";
   };
 in
 {
   #TODO: This not applying?
-  snowfallorg.user.arichtman.home.config.home.file."_homes_x86_64-darwin_arichtman@macbookpro_default.nix".text = "";
+  snowfallorg.user.arichtman.home.config.home.file."_homes_x86_64-darwin_arichtman@macbook-pro_default.nix".text = "";
   home = {
     stateVersion = "22.11";
     #TODO: Why is this not taking effect...
     # I think it's going to bashrc and not zshrc
     # But the docs say it should be for all shells...
     shellAliases = myShellAliases;
-    file."_homes_x86_64-darwin_arichtman@macbookpro_default.nix".text = "";
+    file."_homes_x86_64-darwin_arichtman@macbook-pro_default.nix".text = "";
     #TODO claims home.programs doesn't exist
     # programs.zsh.shellAliases = myShellAliases;
     # Also fails
--- systems/x86_64-darwin/macbook-pro/default.nix
+++ systems/x86_64-darwin/macbook-pro/default.nix
@@ -3,24 +3,25 @@
 , config
 , ...
 }: {
-  networking.hostName = "macbookpro";
+  networking.hostName = "macbook-pro";
 
   #TODO: Determine if this is supposed to be used. Feels like this should be pure system stuff?
   # So... does the user value here translate to which user@system home gets applied?
   # Or should this be a self reference to config.user
-  snowfallorg.user.arichtman.home.config.home.file."_systems_x86_64-darwin_macbookpro_default.nix".text = "";
+  snowfallorg.user.arichtman.home.config.home.file."_systems_x86_64-darwin_macbook-pro_default.nix".text = "";
   # This is bombing claiming the key isn't there...
   # snowfallorg.user.arichtman.home.config.darwin.enable = true;
 
   snowfallorg.user.arichtman.home.config = {
-    default-home = {
-      username = "arichtman";
+    #TODO: Same fix as work mbp
+    # default-home = {
+    #   username = "arichtman";
 
-      git = {
-        email = "10679234+arichtman@users.noreply.github.com";
-        username = "Richtman, Ariel";
-      };
-    };
+    #   git = {
+    #     email = "10679234+arichtman@users.noreply.github.com";
+    #     username = "Richtman, Ariel";
+    #   };
+    # };
   };
 
   home-manager = {
