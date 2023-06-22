a2277f42bea4539bd8c0fd48627c021b7173e077
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2023-06-23 02:21:24 +1000

    hopeful

--- systems/x86_64-linux/work-laptop/default.nix
+++ systems/x86_64-linux/work-laptop/default.nix
@@ -5,19 +5,20 @@
 }:
 with lib; {
   networking.hostName = "work-laptop";
-
   #TODO: move to mac only
   services.yubikey-agent.enable = true;
-
-  snowfallorg.user.arichtman.home.config = {
-    default-home = {
-      username = "nixos";
-      git.email = "Ariel.Richtman@SilverRailTech.com";
-      git.username = "Ariel Richtman";
+  snowfallorg.user.arichtman = {
+    name = "arichtman";
+    home.config = {
+      default-home = {
+        username = "nixos";
+        git.email = "Ariel.Richtman@SilverRailTech.com";
+        git.username = "Ariel Richtman";
+      };
+      #TODO: remove after development
+      file."_systems.x86_64-linux_work-laptop_default.nix".text = "";
+      # isNormalUser = true;
+      # wsl-system.enable = true;
     };
-    #TODO: remove after development
-    file."_systems.x86_64-linux_work-laptop_default.nix".text = "";
-    # isNormalUser = true;
-    # wsl-system.enable = true;
   };
 }
