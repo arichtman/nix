22de23b4131ca61c1eb9a812ce778c7ee528ea15
Richtman, Ariel <10679234+arichtman@users.noreply.github.com>
2023-09-02 13:10:47 +1000

    updated macs to pull lima off the ancient one

--- systems/aarch64-darwin/bne-nb-ariel/default.nix
+++ systems/aarch64-darwin/bne-nb-ariel/default.nix
@@ -1,8 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   networking.hostName = "bne-nb-ariel";
 
   # Add ability to used TouchID for sudo authentication
   security.pam.enableSudoTouchIdAuth = true;
+
+  environment.systemPackages = [
+    pkgs.lima
+  ];
 
   system.stateVersion = 4;
 }
