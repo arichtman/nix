30d89bf6893be1e9c5bbf0a2da36aba38f0e34d3
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2023-09-02 18:15:55 +1000

    simplified further

--- shells/myshell/default.nix
+++ shells/myshell/default.nix
@@ -1,27 +1,6 @@
 {
-  pkgs,
   mkShell,
   ...
 }:
 mkShell {
-  meta.platforms = ["aarch64-darwin" "x86_64-linux" "x86_64-darwin"];
-  packages = with pkgs; [
-    # Minimal development stuff
-    git
-    ripgrep
-    helix
-    pre-commit
-    deploy-rs
-    # Kubernetes stuff
-    kubectl
-    kubectx
-    # Certificates and secrets
-    xkcdpass
-    step-cli
-    openssl
-  ];
-  shellHook = ''
-    pre-commit install --install-hooks
-    echo "Entering the nix  Z O N E"
-  '';
 }
