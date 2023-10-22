6eac3a6495ce2b3de9f3651a02a6b7caab048e8e
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2023-10-22 14:37:13 +1000

    added ssh config for home

--- homes/x86_64-linux/nixos@bruce-banner/default.nix
+++ homes/x86_64-linux/nixos@bruce-banner/default.nix
@@ -4,6 +4,35 @@
   config,
   ...
 }: {
+  programs.ssh.enable = true;
+  programs.ssh.matchBlocks = {
+    proxmox = {
+      hostname = "proxmox.local";
+      user = "root";
+    };
+    github = {
+      hostname = "github.com";
+      user = "git";
+    };
+    probics = {
+      user = "User";
+      hostname = "probics.ddns.net";
+      identityFile = "~/.ssh/probics-home";
+      port = 2222;
+      localForwards = [
+        {
+          bind.address = "";
+          bind.port = 5000;
+          host.address = "localhost";
+          host.port = 3389;
+        }
+      ];
+    };
+    router = {
+      user = "fang";
+      hostname = "router.local";
+    };
+  };
   default-home = {
     username = "nixos";
 
