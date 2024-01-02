b7c08ed16200aad4f9c071b04b7da0d3302c60c8
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-01-02 20:35:24 +1000

    fixed controller disk uuid

--- systems/x86_64-linux/fat-controller/hardware-configuration.nix
+++ systems/x86_64-linux/fat-controller/hardware-configuration.nix
@@ -12,7 +12,7 @@
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod"];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/0951e51a-d458-48f0-89e2-698d21cb159b";
+    device = "/dev/disk/by-uuid/bdef5c05-ae5b-49f0-b2c4-27f0c57e82c5";
     fsType = "ext4";
   };
 }
