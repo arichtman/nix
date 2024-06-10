57d12a8e84bd1fab999e00e36ceafd74fee5a730
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-06-10 18:20:10 +1000

    updated hw config for vms

--- modules/nixos/virtual-node/default.nix
+++ modules/nixos/virtual-node/default.nix
@@ -26,6 +26,9 @@ in
       # ];
 
       boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod"];
+      boot.initrd.kernelModules = [];
+      boot.kernelModules = [];
+      boot.extraModulePackages = [];
 
       fileSystems."/" = {
         device = "/dev/sda1";
