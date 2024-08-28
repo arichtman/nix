7025cde1378cf3dfb594f1060e8b8115884af6fe
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-08-28 12:58:20 +1000

    welcome 2 new idiots to the family

---
+++ systems/x86_64-linux/tweedledum/default.nix
@@ -0,0 +1,11 @@
+_: {
+  networking.hostName = "tweedledum";
+  system.stateVersion = "23.11";
+  lab-node.enable = true;
+  physical-node = {
+    volumes = {
+      bootUuid = "BE29-C60E";
+      rootUuid = "9003cbf5-0177-468a-a534-18f0fbaa2997";
+    };
+  };
+}
