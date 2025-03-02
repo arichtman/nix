a6f4e694c0afec538455e35f5645687aaa545069
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2025-03-02 11:43:29 +1000

    feat: parallelized helper label script

--- label.sh
+++ label.sh
@@ -1,18 +1,20 @@
 #!/bin/env bash
 
-kubectl label no/fat-controller.systems.richtman.au node-role.kubernetes.io/master=master
-kubectl label no/fat-controller.systems.richtman.au kubernetes.richtman.au/ephemeral=false
+kubectl label no/fat-controller.systems.richtman.au node-role.kubernetes.io/master=master &
+kubectl label no/fat-controller.systems.richtman.au kubernetes.richtman.au/ephemeral=false &
 
-kubectl label no/mum.systems.richtman.au node-role.kubernetes.io/worker=worker
-kubectl label no/mum.systems.richtman.au kubernetes.richtman.au/ephemeral=false
+kubectl label no/mum.systems.richtman.au node-role.kubernetes.io/worker=worker &
+kubectl label no/mum.systems.richtman.au kubernetes.richtman.au/ephemeral=false &
 
-kubectl label no/patient-zero.systems.richtman.au node-role.kubernetes.io/worker=worker
-kubectl label no/patient-zero.systems.richtman.au kubernetes.richtman.au/ephemeral=true
-kubectl label no/dr-singh.systems.richtman.au node-role.kubernetes.io/worker=worker
-kubectl label no/dr-singh.systems.richtman.au kubernetes.richtman.au/ephemeral=true
-kubectl label no/smol-bat.systems.richtman.au node-role.kubernetes.io/worker=worker
-kubectl label no/smol-bat.systems.richtman.au kubernetes.richtman.au/ephemeral=true
-kubectl label no/tweedledee.systems.richtman.au node-role.kubernetes.io/worker=worker
-kubectl label no/tweedledee.systems.richtman.au kubernetes.richtman.au/ephemeral=true
-kubectl label no/tweedledum.systems.richtman.au node-role.kubernetes.io/worker=worker
-kubectl label no/tweedledum.systems.richtman.au kubernetes.richtman.au/ephemeral=true
+kubectl label no/patient-zero.systems.richtman.au node-role.kubernetes.io/worker=worker &
+kubectl label no/patient-zero.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/dr-singh.systems.richtman.au node-role.kubernetes.io/worker=worker &
+kubectl label no/dr-singh.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/smol-bat.systems.richtman.au node-role.kubernetes.io/worker=worker &
+kubectl label no/smol-bat.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/tweedledee.systems.richtman.au node-role.kubernetes.io/worker=worker &
+kubectl label no/tweedledee.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/tweedledum.systems.richtman.au node-role.kubernetes.io/worker=worker &
+kubectl label no/tweedledum.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+
+wait
