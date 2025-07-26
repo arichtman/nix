ca712ef01a45b2640b5c1ca7b3bf57b7b38d7834
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2025-07-27 09:48:48 +1000

    wip: rename my labels

--- label.sh
+++ label.sh
@@ -1,17 +1,17 @@
 #!/bin/env bash
 
 kubectl label no/fat-controller.systems.richtman.au node-role.kubernetes.io/master=master &
-kubectl label no/fat-controller.systems.richtman.au kubernetes.richtman.au/ephemeral=false &
+kubectl label no/fat-controller.systems.richtman.au k8s.richtman.au/ephemeral=false &
 
 kubectl label no/patient-zero.systems.richtman.au node-role.kubernetes.io/worker=worker &
-kubectl label no/patient-zero.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/patient-zero.systems.richtman.au k8s.richtman.au/ephemeral=true &
 kubectl label no/dr-singh.systems.richtman.au node-role.kubernetes.io/worker=worker &
-kubectl label no/dr-singh.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/dr-singh.systems.richtman.au k8s.richtman.au/ephemeral=true &
 kubectl label no/smol-bat.systems.richtman.au node-role.kubernetes.io/worker=worker &
-kubectl label no/smol-bat.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/smol-bat.systems.richtman.au k8s.richtman.au/ephemeral=true &
 kubectl label no/tweedledee.systems.richtman.au node-role.kubernetes.io/worker=worker &
-kubectl label no/tweedledee.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/tweedledee.systems.richtman.au k8s.richtman.au/ephemeral=true &
 kubectl label no/tweedledum.systems.richtman.au node-role.kubernetes.io/worker=worker &
-kubectl label no/tweedledum.systems.richtman.au kubernetes.richtman.au/ephemeral=true &
+kubectl label no/tweedledum.systems.richtman.au k8s.richtman.au/ephemeral=true &
 
 wait
