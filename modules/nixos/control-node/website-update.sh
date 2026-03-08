c07c196794c722d0344ab1577b02804f8ad52e9f
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2026-03-08 18:29:25 +1000

    bug: website update account for git submodules

--- modules/nixos/control-node/website-update.sh
+++ modules/nixos/control-node/website-update.sh
@@ -5,6 +5,9 @@ echo "In $(pwd)"
 echo "Starting git pull"
 git pull
 echo "Git pulled"
+echo "Ensuring submodules"
+git submodule update --init --recursive
+echo "Submodules initialized"
 echo "Entering shell and building..."
 nix develop --command zola build --force --output-dir ../www
 echo "Completed!"
