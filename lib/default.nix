2bd32e0a931cc7a5f364cb9ee36ab6986d9c5c89
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2026-03-15 13:32:36 +1000

    chore: change home v4 subnet

--- lib/default.nix
+++ lib/default.nix
@@ -66,7 +66,7 @@ in rec {
       routerGlobalUnicastAddress = "${net.ip6.prefix}:0:${net.ip6.routerEUI64}";
     };
     ip4 = {
-      subnet = "192.168.1.0/24";
+      subnet = "10.128.1.0/24";
     };
   };
   # Pass-through the function in case people want plain gitignores
