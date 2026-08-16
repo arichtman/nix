29ab6a21419caa83966d9a7dc33c610cd4db2029
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2026-08-16 12:14:43 +1000

    [skip ci]wip, default nixos user

--- modules/den/aspects/defaultUser.nix
+++ modules/den/aspects/defaultUser.nix
@@ -3,6 +3,13 @@
     nixos = {
       users.users.nixos = {
         isNormalUser = true;
+        createHome = true;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJt9zJAxzYEK7Y2FYmwkT4cnYr/e4lO2w/ivNL74Pp6B"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMB0EONXbHFqCgHpvJDtFVrDyJeNVHb+XeweP+vYHf0F"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxGxm6tCZlV3vJ6+yAkmQKcqVagfhgaf2aHzVQHvay+"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMSzoJ8/RDgYda2iQN1o8NmqbZnqQFnPLfuAYEaRIcGT"
+        ];
         # Not required due to den.batteries.primary-user
         # extraGroups = ["wheel"];
       };
