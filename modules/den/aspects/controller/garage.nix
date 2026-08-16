dad2f5debee17eb6afe598948181d4af288b571b
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2026-08-16 17:46:36 +1000

    [skip ci]wip, fix garage

--- modules/den/aspects/controller/garage.nix
+++ modules/den/aspects/controller/garage.nix
@@ -3,13 +3,18 @@
     nixos = {
       host,
       pkgs,
+      config,
       ...
     }: {
       services = {
         garage = {
           enable = true;
+          environmentFile = "${config.services.garage.settings.metadata_dir}/.env";
+          # GARAGE_RPC_SECRET=$(openssl rand -hex 32)
+          # GARAGE_ADMIN_TOKEN=$(openssl rand -hex 32)
           settings = {
             replication_factor = 1;
+            # TODO: set rpc_public_addr, as logs suggest
             rpc_bind_addr = "[::]:3901";
             s3_api = {
               api_bind_addr = "[::]:3900";
