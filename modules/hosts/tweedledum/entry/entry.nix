e5ac8a54ff9dda650b5199ca6034c7424f2ac0f0
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2026-08-16 12:04:23 +1000

    [skip ci]wip, tweedledum

---
+++ modules/hosts/tweedledum/entry/entry.nix
@@ -0,0 +1,11 @@
+{
+  den.hosts.x86_64-linux = {
+    tweedledum = {
+      systemSettings.stateVersion = "23.11";
+      settings.volumes = {
+        bootUuid = "BE29-C60E";
+        rootUuid = "9003cbf5-0177-468a-a534-18f0fbaa2997";
+      };
+    };
+  };
+}
--- systems/x86_64-linux/fat-controller/default.nix
+++
@@ -1,9 +0,0 @@
-{...}: {
-  networking.hostName = "fat-controller";
-  system.stateVersion = "24.11";
-  services = {
-    spire-server.enable = true;
-    spire-agent.enable = false; # Needs a new join token on deployment
-    spire-agent.joinToken = "1cd5bbf1-e6e5-4cc8-9098-46dbb2f42755";
-  };
-}
