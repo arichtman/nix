bf9b95e19f430be1a270e032dfa2a72c8106c31d
Ariel Richtman <git@richtman.au>
2026-09-05 17:56:41 +1000

    chore: fix basic deploy method

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-for node in $(cat nodes.txt); do nixos-rebuild switch --build-host $node --target-host $node --flake . --sudo ; done
+for node in $(cat nodes.txt); do
+nixos-rebuild test --build-host "nixos@${node}.systems.richtman.au" --target-host "nixos@${node}.systems.richtman.au" --flake ".#${node}" --sudo ;
+done
